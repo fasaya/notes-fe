005c652ee9f242aa28f3591f6dfccecb0847f78c
Andi Fasaya Yaqhsya Ma'ruf <53859179+fasaya@users.noreply.github.com>
2023-06-20 19:28:51 +0800

    init

---
+++ pages/notes/index.vue
@@ -0,0 +1,18 @@
+<template>
+	<div>
+		<div class="container mx-auto">
+			<div class="grid grid-cols-2">
+				<div>01</div>
+				<!-- ... -->
+				<div>09</div>
+				<!-- ... -->
+			</div>
+		</div>
+	</div>
+</template>
+
+<script setup>
+definePageMeta({
+	path: '/'
+})
+</script>
