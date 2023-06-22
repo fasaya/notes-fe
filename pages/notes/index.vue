4b610ab5b8341b6a0744c6b1950dde515ec3c6bf
Andi Fasaya Yaqhsya Ma'ruf <53859179+fasaya@users.noreply.github.com>
2023-06-22 21:11:10 +0800

    note crate and update

--- pages/notes/index.vue
+++ pages/notes/index.vue
@@ -1,5 +1,6 @@
 <script>
 import { ref } from "vue";
+import axios from "axios";
 
 import Card from "@/components/notes/Card.vue";
 import FormModal from "@/components/notes/FormModal.vue";
@@ -13,18 +14,21 @@ export default {
 			data: {},
 			dataSelected: ref({}),
 			links: {},
+			formSubmitted: false,
 		};
 	},
 	async mounted() {
-		const config = useRuntimeConfig();
-		let url = config.public.apiUrl + "api/v1/note";
-
 		// console.log("url", url);
 
-		this.getNotes(url);
+		this.getNotes();
 	},
 	methods: {
-		getNotes: async function (url) {
+		getNotes: async function (url = null) {
+			if (!url) {
+				const config = useRuntimeConfig();
+				url = config.public.apiUrl + "api/v1/note";
+			}
+
 			await fetch(url)
 				.then((response) => response.json())
 				.then(async (data) => {
@@ -47,6 +51,56 @@ export default {
 		setSelectedData(value) {
 			this.dataSelected = value;
 			console.log("setSelectedData", this.dataSelected);
+		},
+		submitForm() {
+			const config = useRuntimeConfig();
+			let url =
+				config.public.apiUrl +
+				"api/v1/note" +
+				(this.dataSelected.uuid ? "/" + this.dataSelected.uuid : "");
+
+			this.formSubmitted = true;
+
+			// console.log("posturl", url);
+			// console.log("submitEvent", this.$refs.title.value);
+
+			let formData = {
+				title: this.$refs.title.value,
+				body: this.$refs.body.value,
+			};
+			console.log("formData", formData);
+
+			if (this.dataSelected.uuid) {
+				axios
+					.put(url, formData)
+					.then((res) => {
+						this.onCloseModal(false);
+						console.log("res", res.data);
+						this.getNotes();
+					})
+					.catch((err) => {
+						console.log(err);
+					})
+					.finally(() => {
+						//Perform action in always
+					});
+			} else {
+				axios
+					.post(url, formData)
+					.then((res) => {
+						this.onCloseModal(false);
+						console.log("res", res.data);
+						this.getNotes();
+					})
+					.catch((err) => {
+						console.log(err);
+					})
+					.finally(() => {
+						//Perform action in always
+					});
+			}
+
+			this.formSubmitted = false;
 		},
 	},
 	setup() {
@@ -73,6 +127,10 @@ definePageMeta({
 						type="text"
 						placeholder="+ create new notes"
 						class="px-3 py-3 placeholder-medium-stone text-slate-600 relative bg-light-stone-2 rounded text-base border-0 outline-none focus:outline-none focus:ring w-full pl-10 hover:cursor-pointer"
+						@click="
+							showModal = !showModal;
+							setSelectedData({});
+						"
 					/>
 				</div>
 			</div>
@@ -96,10 +154,10 @@ definePageMeta({
 	</div>
 
 	<FormModal :show="showModal" @closed="onCloseModal">
-		<form>
+		<form @submit.prevent="submitForm">
 			<div class="mb-3">
 				<input
-					id="title"
+					ref="title"
 					type="text"
 					placeholder="Title"
 					:value="dataSelected.title"
@@ -108,12 +166,20 @@ definePageMeta({
 			</div>
 			<div class="mb-3">
 				<textarea
-					id="body"
+					ref="body"
 					row="3"
 					placeholder="Description"
 					:value="dataSelected.body"
 					class="px-3 py-3 placeholder-slate-300 text-slate-600 relative bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
 				></textarea>
+			</div>
+			<div class="mb-3">
+				<button
+					class="bg-zinc-500 text-white active:bg-zinc-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+					type="submit"
+				>
+					{{ dataSelected.uuid ? "Update" : "Create" }}
+				</button>
 			</div>
 		</form>
 	</FormModal>
